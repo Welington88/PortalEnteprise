3e3e4ae584f5f1fa500c05a8fde7dd12263f15bc
Welington Marquezini Valverde Da Silva <51893051+Welington88@users.noreply.github.com>
2020-03-20 11:07:54 -0300

    Update UrlDb.php

--- _model/UrlDb.php
+++ _model/UrlDb.php
@@ -8,8 +8,6 @@ class UrlBD {
     private $dsn = "mysql:host=localhost;dbname=alunos"; 
     private $username = "root"; 
     private $passwd = "";
-    private $root = array("welington_marquezini88@live.com",
-        "cintiapaixao.social@gmail.com","miria_2403@hotmail.com");
             
     function __construct() {
         
