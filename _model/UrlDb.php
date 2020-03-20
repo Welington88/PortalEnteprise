dd9976d6f40a0b7f6c6acb93dc6f7ccc9f3215d8
Welington Marquezini <welington_marquezini88@live.com>
2020-03-20 10:59:20 -0300

    modificar UrlDB

--- _model/UrlDb.php
+++ _model/UrlDb.php
@@ -5,12 +5,9 @@
  * @author welin
  */
 class UrlBD {
-    private $dsn = "mysql:host=mysql.hostinger.com.br;dbname=u154126749_aluno";
-    private $username = "u154126749_root";
-    private $passwd = "@wel1988";
-    /*private $dsn = "mysql:host=localhost;dbname=alunos"; 
+    private $dsn = "mysql:host=localhost;dbname=alunos"; 
     private $username = "root"; 
-    private $passwd = "";*/
+    private $passwd = "";
     private $root = array("welington_marquezini88@live.com",
         "cintiapaixao.social@gmail.com","miria_2403@hotmail.com");
             
