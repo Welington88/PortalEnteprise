3b85bb561ad0bb118957a44bcbfc29f972f3432a
Welington Marquezini <welingtonmarquezini@MacBook-Air-de-Welington.local>
2021-02-05 16:59:00 -0300

    usuario root azure

--- _model/UrlDb.php
+++ _model/UrlDb.php
@@ -8,7 +8,7 @@ class UrlBD {
     private $dsn = "mysql:host=localhost:51337;dbname=localdb"; 
     private $username = "azure"; 
     private $passwd = "6#vWHD_$";
-            
+    private $root = array("welington_marquezini88@live.com");         
     function __construct() {
         
     }
